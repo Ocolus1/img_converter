616c2b0a4a3ed9e905ec0fd6f3027a1f99ad304f
Paul Awolesi <awolesiboluwatife@gmail.com>
2022-08-11 16:11:58 +0100

    initial commit

---
+++ img_converter/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from img_converter.users.models import User
+from img_converter.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
---
+++ img_converter/main/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from .views import download_file, index, login_user, logout_user, register, privacy_policy, terms_cond, cookie_policy
+
+app_name = "main"
+urlpatterns = [
+    path("", index, name="index"),
+    path("download/<file>/<filename>", download_file, name="download"),
+    path("register", register, name="register"),
+    path("login_user", login_user, name="login_user"),
+    path("logout_user", logout_user, name="logout_user"),
+    path("privacy_policy", privacy_policy, name="privacy_policy"),
+    path("terms_and_condition", terms_cond, name="terms_and_condition"),
+    path("cookie_policy", cookie_policy, name="cookie_policy"),
+]
